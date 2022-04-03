fead555f46884567389ca8aba4d9a0de17f40639
onetrue5200 <oneruexie@gmail.com>
2022-04-03 12:24:13 +0800

    rename a model

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_test = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
