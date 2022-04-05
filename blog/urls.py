dd2a9f7b28f9dd03045f9085cf26d0e547e65fe5
onetrue5200 <oneruexie@gmail.com>
2022-04-05 16:27:42 +0800

    import boostrap

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('test/', views.Test.as_view(), name='test'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
